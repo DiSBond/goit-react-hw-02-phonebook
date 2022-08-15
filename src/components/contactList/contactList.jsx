e83a04b5cc4af83e57fdd38d37d6a05e4d7da305
DiSBond <87495312+DiSBond@users.noreply.github.com>
2022-08-15 17:13:03 +0300

    1508

--- src/components/contactList/contactList.jsx
+++ src/components/contactList/contactList.jsx
@@ -2,26 +2,24 @@ import { ContactListStl } from './contactList.styled';
 import ContactListElement from 'components/contactElement/contactElement';
 import propTypes from 'prop-types';
 
-function ContactList(array, deleteContact) {
+const ContactList = ({ array, deleteContact }) => {
   return (
-    <div>
-      <ContactListStl>
-        {array.map(({ id, name, number }) => {
-          return (
-            <li key={id}>
-              <ContactListElement
-                id={id}
-                name={name}
-                number={number}
-                onDelete={deleteContact}
-              />
-            </li>
-          );
-        })}
-      </ContactListStl>
-    </div>
+    <ContactListStl>
+      {array.map(({ id, name, number }) => {
+        return (
+          <li key={id}>
+            <ContactListElement
+              id={id}
+              name={name}
+              number={number}
+              onDelete={deleteContact}
+            />
+          </li>
+        );
+      })}
+    </ContactListStl>
   );
-}
+};
 
 export default ContactList;
 
